6d1be33387c09f1c03ede7fec8814444737bfc8b
Jordan Searcy-Hosea <o7endofwatch@mailfence.com>
2022-11-30 09:48:47 -1000

    Implement server skeleton

---
+++ server/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+const PORT = process.env.PORT || 1337;
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:/${PORT}`);
+});
