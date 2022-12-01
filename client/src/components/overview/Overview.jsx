009c3f7d4a2661216a9ad5fac727ee79a82d485e
Warren Siu <warren.siu1@gmail.com>
2022-12-01 10:58:30 -0800

    Added states for review and thumbnails

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,15 +1,41 @@
-import React, {useEffect, useState, useMemo} from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
-const Overview = ({currentProduct, request}) => {
+const Overview = ({ currentProduct, request }) => {
 
   const [ratings, setRatings] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [thumbnails, setThumbnails] = useState([]);
 
-  request(`/reviews/?product_id=${currentProduct.id}`, {}, )
+  if (currentProduct !== null && reviews.length === 0) {
+    request(`/reviews/?product_id=${currentProduct.id}`, 'GET', {}, (err, response) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(response.results);
+        setReviews(response.results.map(result => {
+          console.log('i am result.rating: ', result.rating);
+          return result.rating;
+        }));
+      }
+    })
+  }
+
+  if (currentProduct !== null )
 
   return (
-    <div>
-      {currentProduct.name} {currentProduct.default_price} {currentProduct.slogan}
+    <div id="overview">
+      {currentProduct !== null &&
+        <div>
+          {currentProduct.name} {currentProduct.default_price} {currentProduct.slogan}
+        </div>
+      }
+      {reviews.length !== 0 &&
+        reviews.map(review => {
+          <div>
+            {review.rating}
+          </div>
+        })}
     </div>
   )
 }
