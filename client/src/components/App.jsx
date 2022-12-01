d5f7ee69e07d22f7eb11a1542077993e7d1151d9
Jordan Searcy-Hosea <o7endofwatch@mailfence.com>
2022-12-01 07:26:47 -1000

    Remove unnecessary property

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -52,7 +52,7 @@ const App = () => {
   return (
     <>
       <Overview currentProduct={currentProduct} request={request} />
-      <RelatedItems currentProduct={currentProduct} setCurrentProduct={setCurrentProduct} request={request} />
+      <RelatedItems currentProduct={currentProduct} request={request} />
       <QuestionsAnswers currentProduct={currentProduct} request={request} />
       <RatingsReviews currentProduct={currentProduct} request={request} />
     </>
