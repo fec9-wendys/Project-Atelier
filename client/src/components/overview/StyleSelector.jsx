7fd4244390f6bfe6ed9a2115ee8e2480cd709912
Warren Siu <warren.siu1@gmail.com>
2022-12-07 23:32:39 -0800

    added opacity for style selector

--- client/src/components/overview/StyleSelector.jsx
+++ client/src/components/overview/StyleSelector.jsx
@@ -10,9 +10,11 @@ const StyleSelector = ({ currentProductStyle, setCurrentProductStyle, styles }) 
     <div className="grid-container" id="style-selector">
       {styles.map((style, index) => {
         return (
-          <div key={index} className="style-selector-thumbnail">
-            <img style={thumbnailStyle}  alt="No photo available" src={style.photos[0].thumbnail_url} onClick={e => handleClick(index)} />
-
+          <div key={index} className="style-selector-thumbnails">
+            {styles[index] === currentProductStyle ?
+              <img alt="No photo available" src={style.photos[0].thumbnail_url} data-active onClick={e => handleClick(index)} />
+              :  <img alt="No photo available" src={style.photos[0].thumbnail_url} onClick={e => handleClick(index)} />
+            }
             {currentProductStyle.style_id === style.style_id ?
               <div id="checkmark">
                 <i style={{ color: '#08B6EB' }}  className="fa-solid fa-circle-check" />
@@ -28,11 +30,5 @@ const StyleSelector = ({ currentProductStyle, setCurrentProductStyle, styles }) 
 
 }
 
-const thumbnailStyle = {
-  height: 70,
-  width: 70,
-  cursor: 'pointer',
-  borderRadius: '50%'
-};
 
 export default StyleSelector
