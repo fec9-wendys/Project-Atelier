23bb758ac58ccd14405b576629c29af104e3e905
Bo Shao <boshao13@gmail.com>
2022-12-13 16:48:03 -0700

    fixed a few

--- client/src/components/questions-answers/Photos.jsx
+++ client/src/components/questions-answers/Photos.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
+const handleClick = () => {
+  console.log('clicked')
+}
 const Photos = ({photo}) => {
   return (
-    <img src={photo.url} width='100' height='100'/>
+    <img onClick={handleClick}src={photo.url} width='100' height='100'/>
   )
 }
 export default Photos
